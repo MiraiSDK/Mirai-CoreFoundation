c1053f9ef958c22bd1ceb766b33344ac7b086228
Jack <jack@tiny4cocoa.org>
2014-08-10 02:19:09 +0800

    add toolchain_build

---
+++ toolchain_build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+checkError()
+{
+    if [ "${1}" -ne "0" ]; then
+        echo "*** Error: ${2}"
+        exit ${1}
+    fi
+}
+
+./configure --host=arm-linux-androideabi --disable-gcd --disable-implicit-bridge
+checkError $? "configure core-base failed"
+
+make
+checkError $? "build core-base failed"
+
+make install
+
+pushd $MIRAI_SDK_PREFIX/lib
+mv libgnustep-corebase.so.0.2 libgnustep-corebase.so
+rm libgnustep-corebase.so.0
+popd
